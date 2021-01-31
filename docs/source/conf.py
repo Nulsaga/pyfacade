ddbb1b7ac5faa7c6921f26fd81b0639dfb105087
Qi Wang <77314258+Nulsaga@users.noreply.github.com>
2021-01-31 06:22:30 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath("..\\..\\pyfacade\\"))
 sys.path.insert(0, os.path.abspath("..\\..\\"))
-
 
 # -- Project information -----------------------------------------------------
 
